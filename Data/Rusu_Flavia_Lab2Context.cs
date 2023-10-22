20af73852ea652791b86ec0dcb6613fdea9e8256
FLAVIA-MARIA RUSU <flavia.rusu@stud.ubbcluj.ro>
2023-10-22 21:25:36 +0300

    Add project files.

---
+++ Data/Rusu_Flavia_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Rusu_Flavia_Lab2.Models;
+
+namespace Rusu_Flavia_Lab2.Data
+{
+    public class Rusu_Flavia_Lab2Context : DbContext
+    {
+        public Rusu_Flavia_Lab2Context (DbContextOptions<Rusu_Flavia_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Rusu_Flavia_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Rusu_Flavia_Lab2.Models.Publisher>? Publisher { get; set; }
+
+        public DbSet<Rusu_Flavia_Lab2.Models.Author>? Author { get; set; }
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,10 @@
+﻿namespace Rusu_Flavia_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Book> Books { get; set; }
+    }
+}
